957b65fb8682b566870fac0ca3b4a0384fcad8be
GinoSebastianD <118413024+GinoSebastianD@users.noreply.github.com>
2026-08-30 20:14:45 -0500

    Update quadtreeRegion.cpp

--- quadtreeRegion.cpp
+++ quadtreeRegion.cpp
@@ -104,3 +104,175 @@ int main() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#include "iostream"
+//#include "vector"
+//
+//using namespace std;
+//
+//
+//struct Node
+//{
+//	vector<Node*> quad = vector<Node*>(4, nullptr);
+//	int v;
+//	int x;
+//	int y;
+//	int size;
+//	Node(int val,  int x_, int y_ , int tam ): v(val) , x(x_) , y(y_) , size(tam) {}
+//	bool eshoja() {
+//		return quad[0] == nullptr;
+//	}
+//};
+//
+//class QuadtreeRegion
+//{
+//public:
+//
+//	vector<vector<int>> matriz;
+//	int limite;
+//	Node* raiz = nullptr;
+//
+//	QuadtreeRegion(int l) : limite(l) , matriz(l,vector<int>(l,0)){}
+//
+//	void build();
+//	bool esigual(int x, int y , int size);
+//	Node* recursividad_build(int x, int y, int val);
+//	void set(int x, int y, int size);
+//
+//	void print();
+//	void print_recursivo(Node* root);
+//};
+//
+//
+//void QuadtreeRegion::build()
+//{
+//	raiz = nullptr;
+//	raiz = recursividad_build(0,0,limite);
+//}
+//bool QuadtreeRegion::esigual(int x, int y, int size)
+//{
+//	int val = matriz[x][y];
+//	for (int i = x ; i < x + size ; i++)
+//	{
+//		for (int j = y; j < y + size ; j++)
+//		{
+//			if (val != matriz[i][j])
+//			{
+//				return false;
+//			}
+//		}
+//	}
+//	return true;
+//}
+//Node* QuadtreeRegion::recursividad_build(int x, int y, int size)
+//{
+//	
+//	if (size == 1 || esigual(x,y,size))
+//	{
+//		Node* newNode = new Node(matriz[x][y], x, y, size);
+//		return newNode;
+//	}
+//	Node* newNode = new Node(-1, x, y, size);
+//	int medio = size / 2;
+//	newNode->quad[0] = recursividad_build(x, y, medio);
+//	newNode->quad[1] = recursividad_build(x + medio, y, medio);
+//	newNode->quad[2] = recursividad_build(x, y + medio, medio);
+//	newNode->quad[3] = recursividad_build(x + medio, y + medio, medio);
+//	
+//	return newNode;
+//
+//}
+//
+//void QuadtreeRegion::set(int x, int y, int val)
+//{
+//	matriz[x][y] = val;
+//	build();
+//}
+//
+//void QuadtreeRegion::print()
+//{
+//	print_recursivo(raiz);
+//}
+//
+//void QuadtreeRegion::print_recursivo(Node* root)
+//{
+//	if (!root)
+//	{
+//		return;
+//	}
+//	if (root->eshoja())
+//	{
+//		cout << "Hoja " << "(" << root->x << "," << root->y << ") :" << root->v << "\n";
+//	}
+//	else
+//	{
+//		cout <<"Nodo: " << "(" << root->x << "," << root->y << ") :" <<"size: " << root->size << "\n";
+//	}
+//	for (int i = 0 ; i < 4; i++)
+//	{
+//		print_recursivo(root->quad[i]);
+//	}
+//}
+//
+//
+//
+//
+//int main() {
+//
+//	QuadtreeRegion qt(4);
+//	qt.matriz = {
+//	{ 1, 0, 0, 0 },
+//	{1, 1, 1, 0},
+//	{1, 0, 1, 1},
+//	{0, 0, 1, 1}
+//	};
+//
+//	qt.build();
+//	qt.print();
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
