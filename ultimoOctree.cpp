767801757b8f8c13fbb37b45bacbcc676476f2d3
GinoSebastianD <118413024+GinoSebastianD@users.noreply.github.com>
2026-09-18 16:49:41 -0500

    Update ultimoOctree.cpp

--- ultimoOctree.cpp
+++ ultimoOctree.cpp
@@ -1,1 +1,54 @@
 
+#include "iostream"
+#include "vector"
+#include "algorithm"
+#include "fstream"
+#include "string"
+#include "iomanip"
+
+struct Point(){
+    double x;
+    double y;
+    double z;
+    Point(double a = 0 , double b = 0 , double c = 0 ){
+        x = a;
+        y = b;
+        z = c;
+    }
+
+};
+
+
+
+class Octree{
+
+    Octree* hijos[8];
+    vector<Point> puntos;
+    Point esquina_inferior_izquierda;
+    double h;
+    int capacidad;
+    int nivel;
+    int numero_puntos;
+
+
+    Octree(Point esquina = Point(0,0,0) , double lado = 128 , int N = 100 , int profundidad = 0){
+        esquina_inferior_izquierda = esquina;
+        h = lado;
+        capacidad = N;
+        nivel = profundidad;
+        numero_puntos =0;
+        for(int i = 0 ; i < 8 ; i++){
+            hijos[i] = nullptr;
+        }
+    }
+
+
+
+};
+
+
+
+int main(){
+
+
+}
