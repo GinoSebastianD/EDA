e3e170ce741f2b8800b7fdca6797b89cd6aec8a3
GinoSebastianD <118413024+GinoSebastianD@users.noreply.github.com>
2026-09-18 18:49:14 -0500

    Update ultimoOctree.cpp

--- ultimoOctree.cpp
+++ ultimoOctree.cpp
@@ -31,6 +31,7 @@ class Octree{
     int numero_puntos;
 
 
+
     Octree(Point esquina = Point(0,0,0) , double lado = 128 , int N = 100 , int profundidad = 0){
         esquina_inferior_izquierda = esquina;
         h = lado;
@@ -42,7 +43,68 @@ class Octree{
         }
     }
 
+    bool eshoja() const{
+        return hijos[0] == nullptr;
+    }
+
+
+    bool contiene(const Point& p) const{
+        return esquina_inferior_izquierda.x >= p.x && p.x <= esquina_inferior_izquierda + h &&
+               esquina_inferior_izquierda.y >= p.y && p.y <= esquina_inferior_izquierda + h &&
+               esquina_inferior_izquierda.z >= p.z && p.z <= esquina_inferior_izquierda + h;
+    }
+
+    bool insert(const Point& p ){
+        if(eshoja()){
+            
+            
+            
+        }
+    
+
+    }
+
+
 };
+
+
+bool cargararchivo(const string& nombre , int N, Octree*& arbol){
+    ifstream archivo(nombre.c_str());
+    vector<Point> datos;
+    string linea;
+    while(getline(archivo,linea)){
+        istringstream entrada(linea);
+        Point p;
+        if(entrada >> p.x >> p.y >> p.z){
+            datos.push_back((p));
+        }
+    }
+
+    double minX = datos[0].x, maxX = datos[0].x;
+    double minY = datos[0].y, maxY = datos[0].y;
+    double minZ = datos[0].z, maxZ = datos[0].z;
+
+    for (const auto& p : datos) {
+        minX = min(minX, p.x); maxX = max(maxX, p.x);
+        minY = min(minY, p.y); maxY = max(maxY, p.y);
+        minZ = min(minZ, p.z); maxZ = max(maxZ, p.z);
+    }
+    double lado = max({maxX-minX , maxY - minY, maxZ - minZ }) *1.01;
+
+    Point bottomleft(minX,minY,minZ);
+
+
+
+    Octree* nuevo = new Octree(bottomleft , lado, N );
+    int total = 0;
+    for(size_t i = 0; i < datos.size() ; i++){
+        if(nuevo->   ){
+            total++;
+        }
+    }
+
+
+}
 
 
 
